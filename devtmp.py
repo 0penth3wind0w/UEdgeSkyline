08d3b09eca364f368f4f299a5849e5ae60aa0f8f
0penth3wind0w <fc0029@yahoo.com.tw>
2019-05-17 11:24:50 +0800

    update devtmp to draw

--- devtmp.py
+++ devtmp.py
@@ -4,14 +4,14 @@ from matplotlib.patches import Rectangle
 
 from data.dataClass import Data, batchImport
 
-data = batchImport('10_dim2_pos3_rad4_0100.csv', 3)
+data = batchImport('20_dim2_pos5_rad5_0100.csv', 5)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
 for d in data:
-    x = [d.getLocation(i)[0] for i in range(3)]
-    y = [d.getLocation(j)[1] for j in range(3)]
+    x = [d.getLocation(i)[0] for i in range(5)]
+    y = [d.getLocation(j)[1] for j in range(5)]
     ax.scatter(x,y,alpha=0.5)
     ax.add_patch(Rectangle(xy=(d.getLocationMin()[0],d.getLocationMin()[1]),width=d.getLocationMax()[0]-d.getLocationMin()[0], height=d.getLocationMax()[1]-d.getLocationMin()[1], linewidth=1, fill=False))    
 
